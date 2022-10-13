96a0645b0ba1fa5aecaaa19343545e020e11b15b
Daniel Idowu <74189249+dharniel45@users.noreply.github.com>
2022-10-13 10:42:08 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className = "App-header">
+    <h1>Migrate NFT</h1>
+    <h3>Lets move this project from Moralis to Alchemy</h3>
+    <button className = "yes">Mint NFT</button>
     </div>
   );
 }
